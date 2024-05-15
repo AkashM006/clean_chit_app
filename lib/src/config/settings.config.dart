9e292becd0f6ca847ab5e9205b416559a5d1b5a4
Akash M <akashm006@outlook.com>
2024-05-15 22:01:59 +0530

    Changed app config file to change the app inactivity duration

--- lib/src/config/settings.config.dart
+++ lib/src/config/settings.config.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color appSeedColor = Colors.blue.shade700;
-Duration appInactivityDuration = const Duration(seconds: 5);
-// Duration appInactivityDuration = const Duration(minutes: 5);
+Duration appInactivityDuration = const Duration(minutes: 5);
 const appName = "Chit App";
